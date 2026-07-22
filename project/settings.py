c660eec56cf5204f4eea0bdef0d7d04f8b50ca68
gabriel-pagani <gabrielpaganidesouza@gmail.com>
2026-07-22 11:53:44 -0300

    Add LDAP authentication configuration to settings and example env

--- project/settings.py
+++ project/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reversion',
     'app',
 ]
 
@@ -82,6 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTHENTICATION_BACKENDS = [
     'app.backends.EmailModelBackend',
+    'django_auth_ldap.backend.LDAPBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
@@ -102,3 +104,37 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static_root'
 
 AUTH_USER_MODEL = 'app.Users'
+
+# Auth LDAP
+from django_auth_ldap.config import LDAPSearch, ActiveDirectoryGroupType
+from ldap import SCOPE_SUBTREE
+
+AUTH_LDAP_SERVER_URI = os.getenv('AUTH_LDAP_SERVER_URI')
+
+AUTH_LDAP_BIND_DN = os.getenv('AUTH_LDAP_BIND_DN')
+
+AUTH_LDAP_BIND_PASSWORD = os.getenv('AUTH_LDAP_BIND_PASSWORD')
+
+AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
+
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    os.getenv('AUTH_LDAP_USER_SEARCH'),
+    SCOPE_SUBTREE,
+    "(sAMAccountName=%(user)s)"
+)
+
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+}
+
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+
+AUTH_LDAP_MIRROR_GROUPS = True
+
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    os.getenv('AUTH_LDAP_GROUP_SEARCH'),
+    SCOPE_SUBTREE,
+    "(objectClass=group)"
+)
