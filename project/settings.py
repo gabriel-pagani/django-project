001abf7f36777d113e0f903230e9cf395128eef5
Gabriel Pagani <gabrielpaganidesouza@gmail.com>
2026-08-09 01:25:49 -0300

    Enhance security configurations: update Makefile for cleanup commands, modify nginx settings for SSL handling, and add secure settings in Django configuration.

--- project/settings.py
+++ project/settings.py
@@ -125,6 +125,31 @@ AUTH_USER_MODEL = 'app.User'
 
 ADMIN_PANEL_PATH = os.getenv('ADMIN_PANEL_PATH', 'admin')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+USE_X_FORWARDED_HOST = True
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True
+
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_HSTS_SECONDS = 31536000
+
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+    SECURE_HSTS_PRELOAD = True
+
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+
+    SESSION_COOKIE_HTTPONLY = True
+
+    X_FRAME_OPTIONS = 'DENY'
+
+    SECURE_REFERRER_POLICY = 'same-origin'
+
 # Auth LDAP
 from django_auth_ldap.config import LDAPSearch, ActiveDirectoryGroupType
 from ldap import SCOPE_SUBTREE
